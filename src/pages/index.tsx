7c28f024415afe2e61f9402d84a6f0479ab4d1bd
Igor <igor-skromnik@yandex.ru>
2023-05-19 03:16:34 +0300

    add-charts-templates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,214 @@ import Grid from '@mui/material/Grid'
 
 // ** Icons Imports
 // ** Demo Components Imports
-import Trophy from 'src/views/dashboard/Trophy'
+import ReactApexcharts from "../@core/components/react-apexcharts";
+import {ApexOptions} from "apexcharts";
+import {useTheme} from "@mui/material/styles";
+
 
 const Dashboard = () => {
+
+  const theme = useTheme()
+
+  const options: ApexOptions = {
+    chart: {
+      parentHeightOffset: 0,
+      toolbar: { show: false }
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 9,
+        distributed: true,
+        columnWidth: '40%',
+        endingShape: 'rounded',
+        startingShape: 'rounded'
+      }
+    },
+    stroke: {
+      width: 2,
+      colors: [theme.palette.primary.main]
+    },
+    legend: { show: false },
+    grid: {
+      strokeDashArray: 7,
+      padding: {
+        top: -1,
+        right: 0,
+        left: -12,
+        bottom: 5
+      }
+    },
+    dataLabels: { enabled: false },
+    colors: [
+      theme.palette.primary.main,
+      theme.palette.primary.main,
+      theme.palette.primary.main,
+      theme.palette.primary.main,
+      theme.palette.primary.main,
+      theme.palette.primary.main
+    ],
+    states: {
+      hover: {
+        filter: { type: 'none' }
+      },
+      active: {
+        filter: { type: 'none' }
+      }
+    },
+    xaxis: {
+      categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      tickPlacement: 'on',
+      labels: { show: false },
+      axisTicks: { show: false },
+      axisBorder: { show: false }
+    },
+    yaxis: {
+      show: true,
+      tickAmount: 4,
+      labels: {
+        offsetX: -17,
+        formatter: value => `${value > 999 ? `${(value / 1000).toFixed(0)}` : value}k`
+      }
+    }
+  }
+
+  const optionsArea: ApexOptions = {
+    series: [{
+      name: 'series1',
+      data: [31, 40, 28, 51, 42, 109, 100]
+    }, {
+      name: 'series2',
+      data: [11, 32, 45, 32, 34, 52, 41]
+    }],
+    chart: {
+      height: 350,
+      type: 'area'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      type: 'datetime',
+      categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+    },
+    tooltip: {
+      x: {
+        format: 'dd/MM/yy HH:mm'
+      },
+    },
+  };
+
+  const optionsPie: ApexOptions = {
+    series: [44, 55, 13, 33],
+    chart: {
+      width: 380,
+      type: 'donut',
+    },
+    dataLabels: {
+      enabled: false
+    },
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          show: false
+        }
+      }
+    }],
+    legend: {
+      position: 'right',
+      offsetY: 0,
+      height: 230,
+    }
+  };
+
+  const optionsColumn: ApexOptions = {
+    series: [{
+      data: [21, 22, 10, 28, 16, 21, 13, 30]
+    }],
+    chart: {
+      height: 350,
+      type: 'bar',
+      events: {
+        click: ()=>{
+          console.log()}
+      }
+    },
+    colors:  ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+      '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+      '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+      '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+      '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+      '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+      '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+      '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+      '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
+    plotOptions: {
+      bar: {
+        columnWidth: '45%',
+        distributed: true,
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    legend: {
+      show: false
+    },
+    xaxis: {
+      categories: [
+        ['John', 'Doe'],
+        ['Joe', 'Smith'],
+        ['Jake', 'Williams'],
+        'Amber',
+        ['Peter', 'Brown'],
+        ['Mary', 'Evans'],
+        ['David', 'Wilson'],
+        ['Lily', 'Roberts'],
+      ],
+      labels: {
+        style: {
+          colors:  ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+            '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+            '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+            '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+            '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+            '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+            '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+            '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+            '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+            '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
+          fontSize: '12px'
+        }
+      }
+    }
+  };
+
   return (
       <Grid container spacing={6}>
-        <Grid item xs={12} md={4}>
-          <Trophy />
+        <Grid item xs={12} md={6}>
+          <ReactApexcharts type='bar' height={405} options={options} series={[{ data: [37, 57, 45, 75, 57, 40, 65] }]} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ReactApexcharts type='line' height={405} options={options} series={[{ data: [37, 57, 45, 75, 57, 40, 65] }]} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ReactApexcharts type='area' height={405} options={optionsArea} series={[{ data: [37, 57, 45, 75, 57, 40, 65] }]} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ReactApexcharts type='donut' height={405} options={optionsPie} series={[44, 55, 13, 33]} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ReactApexcharts type='bar' height={205} options={optionsColumn} series={[{
+            data: [21, 22, 10, 28, 16, 21, 13, 30]
+          }]} />
         </Grid>
       </Grid>
   )
